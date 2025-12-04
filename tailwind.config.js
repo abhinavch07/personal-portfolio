946546e263fed5bcd327c735d99c990f6c13ab84
Abhinav Choudhary <50580803+abhinavch07@users.noreply.github.com>
2025-12-04 23:49:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",  // This looks for files in the main folder
   ],
   theme: {
     extend: {},
