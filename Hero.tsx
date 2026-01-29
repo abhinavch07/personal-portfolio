2caa500b7676324ce4a954b921cac8115d903404
Abhinav Choudhary <50580803+abhinavch07@users.noreply.github.com>
2026-01-29 16:13:21 +0530

    Revert Hero.tsx to previous version before commit 29fc0c34dbea517b69a037fcb08ced214ec98851

---
+++ Hero.tsx
@@ -0,0 +1,14 @@
+// Previous content of Hero.tsx before commit 29fc0c34dbea517b69a037fcb08ced214ec98851
+
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <h1>Welcome to My Portfolio</h1>
+            <p>Here you'll find all of my projects and skills.</p>
+        </div>
+    );
+};
+
+export default Hero;
