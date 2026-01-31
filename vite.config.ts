37f98144afe471daf832a34134a3e79a8be56770
Abhinav Choudhary <50580803+abhinavch07@users.noreply.github.com>
2026-01-31 22:56:34 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,13 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', 'VITE_');
+    loadEnv(mode, '.');
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
-        'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
